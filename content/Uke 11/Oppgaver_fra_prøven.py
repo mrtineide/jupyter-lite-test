e1c2c0429935d7e7841f3c4ea01dc8af9f84fe3c
Martin <43970264+mrtineide@users.noreply.github.com>
2024-03-13 12:42:05 +0100

    Add additional tasks from test

--- content/Uke 11/Oppgaver_fra_prøven.py
+++ content/Uke 11/Oppgaver_fra_prøven.py
@@ -1,3 +1,32 @@
+# Med elif - altså ellers hvis
+# Oppgave: Hva blir printet ut? (linje 13)
+tall = 10
+
+if tall <= 0:
+    svar = "ingen"
+elif tall <= 5:
+    svar = "noen"
+elif tall <= 10:
+    svar = "flere"
+elif tall <= 100:
+    svar = "mange"
+print(svar)
+
+# Uten elif - altså uten ellers hvis
+# Oppgave: Hva blir printet ut? (linje 27)
+tall = 10
+
+if tall <= 0:
+    svar = "ingen"
+if tall <= 5:
+    svar = "noen"
+if tall <= 10:
+    svar = "flere"
+if tall <= 100:
+    svar = "mange"
+print(svar)
+
+# Oppgave: Forklar hva denne koden gjør
 alder = int(input("Hvor gammel er du?"))
 
 if alder < 18:
@@ -7,6 +36,7 @@ else:
 
 
 # Koden under er en modifisert utgave av et svar på oppgaven fra https://codingbat.com/prob/p135815
+# Oppgave: forklar hva koden gjør
 temp = 60 
 is_summer = False
 
